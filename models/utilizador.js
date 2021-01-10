d04887cd5a105383bc6448e86bd2bb865f2c5100
Daniel <danielvice988g@gmail.com>
2021-01-10 17:54:14 +0000

    Sistema de login adicionado

--- models/utilizador.js
+++ models/utilizador.js
@@ -1,1 +1,12 @@
-/* Exemplo */
+class utilizador
+{
+    constructor(id,nome,email,privilegio)
+    {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.privilegio = privilegio;
+    }
+
+}
+module.exports = utilizador;
