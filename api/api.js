3a3b82043076f85df2d094b705d513fb77bc2dfb
Daniel <danielvice988g@gmail.com>
2021-01-11 17:54:50 +0000

    Login api

--- api/api.js
+++ api/api.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const queries = require("../operators/queries");
+const utilizador = require("../models/utilizador")
 const api = express.Router();
 
 api.post('/registos', async function(request, response) {
@@ -16,5 +17,29 @@ api.post('/registos', async function(request, response) {
         response.end();
     }
 })
+
+api.post('/applogin', async function(request, response) {
+    console.log(request.query);
+    var useremail = request.query.user_email;
+    var userpassword = request.query.user_password;
+    if (useremail && userpassword) {
+        let query = new queries();
+        let results = 0;
+        try{
+        results = await query.verificarUtilizador(useremail,userpassword);
+        }catch(err){
+            console.log(err);
+        }
+        //console.log(results);
+        if (results.length > 0){
+            let user = new utilizador(results[0].user_id, results[0].user_nome, results[0].user_email, results[0].user_privilegio);
+            response.json(user);
+            response.end();
+        } else {
+            response.message = 'Invalid username or password';
+            response.end();
+        }           
+    };
+});
 
 module.exports = api;
